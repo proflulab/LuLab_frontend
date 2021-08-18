f2f8981af6cf7bd76be2a9c257102785d12f84d1
shimingy-zx <shiming.y@qq.com>
2021-08-18 14:10:00 +0800

    no message

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: "Lu-Lab",
+      title: "LuLab",
       theme: ThemeData.light(),
       home: Home(),
     );
