60fbc56b69fe6c05cd746dedba6f45ec53cfe24f
杨仕明 <shiming.y@qq.com>
2021-09-24 21:33:40 +0800

    添加阿里巴巴矢量图标库引用方式

---
+++ lib/services/myIcon.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/widgets.dart';
+
+class MyIcon {
+  static const String _family = 'MyIcons';
+  MyIcon._();
+  static const IconData icon_zan = IconData(0xe829, fontFamily: _family);
+  static const IconData icon_xiazai = IconData(0xe652, fontFamily: _family);
+  static const IconData icon_huiyuan = IconData(0xe61d, fontFamily: _family);
+  static const IconData icon_fankui = IconData(0xe62d, fontFamily: _family);
+  static const IconData icon_yuyin = IconData(0xe606, fontFamily: _family);
+  static const IconData icon_dyvmsyuyinfuwu =
+      IconData(0xe6e8, fontFamily: _family);
+}
