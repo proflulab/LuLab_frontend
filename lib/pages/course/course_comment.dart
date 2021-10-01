d044d4857a2e7f0dacda6e1ea2e5367a2de9d581
lin92n <2326182533@qq.com>
2021-10-01 15:07:24 +0000

    课程页面

---
+++ lib/pages/course/course_comment.dart
@@ -0,0 +1,244 @@
+import 'package:flutter/material.dart';
+
+/// 课程评论页面
+
+class CourseCommentPage extends StatelessWidget {
+  const CourseCommentPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: CourseComment(),
+    );
+  }
+}
+
+class CourseComment extends StatefulWidget {
+  CourseComment({Key? key}) : super(key: key);
+
+  @override
+  _CourseCommentState createState() => _CourseCommentState();
+}
+
+class _CourseCommentState extends State<CourseComment> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Wrap(
+        children: [
+          //  评价顶部
+          Container(
+            margin: EdgeInsets.symmetric(vertical: 20),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              mainAxisSize: MainAxisSize.max,
+              children: [
+                Text('综合评分：'),
+                Row(
+                  children: [
+                    Icon(
+                      Icons.star,
+                      color: Colors.orange,
+                      size: 25,
+                    ),
+                    Icon(
+                      Icons.star,
+                      size: 25,
+                      color: Colors.orange,
+                    ),
+                    Icon(
+                      Icons.star,
+                      color: Colors.orange,
+                      size: 25,
+                    ),
+                    Icon(
+                      Icons.star,
+                      color: Colors.orange,
+                      size: 25,
+                    ),
+                    Icon(
+                      Icons.star,
+                      size: 25,
+                      color: Colors.white,
+                    ),
+                  ],
+                ),
+                Text(
+                  "4.0分",
+                  style: TextStyle(fontSize: 16),
+                ),
+              ],
+            ),
+          ),
+          SizedBox(
+            height: 50,
+          ),
+          // 评论列表
+          Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Text(
+                    '全部评价(30)',
+                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.w500),
+                  ),
+                ],
+              ),
+              Container(
+                padding: EdgeInsets.only(top: 30),
+                height: 500,
+                child: ListView(
+                  scrollDirection: Axis.vertical,
+                  children: [
+                    buildEachCourseComment(),
+                    buildEachCourseComment(),
+                    buildEachCourseComment(),
+                    buildEachCourseComment(),
+                    buildEachCourseComment(),
+                    buildEachCourseComment(),
+                    buildEachCourseComment(),
+                    buildEachCourseComment(),
+                    buildEachCourseComment(),
+                    buildEachCourseComment(),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  Container buildEachCourseComment() {
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: 8, horizontal: 3),
+      child: ListTile(
+        leading: CircleAvatar(
+          backgroundImage: AssetImage('images/1.jpg'),
+        ),
+        title: Column(
+          children: [
+            // 头衔
+            buildName(),
+            // 评分
+            buildScore(),
+            // 评论
+            buildCourseComment(),
+            // 时间
+            buildData()
+          ],
+        ),
+      ),
+    );
+  }
+
+  Row buildName() {
+    return Row(
+      children: [
+        Text(
+          '有志青年',
+          style: TextStyle(fontSize: 15),
+        ),
+        SizedBox(
+          width: 10,
+        ),
+        Text(
+          '骨干学员',
+          style: TextStyle(color: Color(0xffcccccc), fontSize: 15),
+        )
+      ],
+    );
+  }
+
+  Row buildScore() {
+    return Row(
+      children: [
+        Icon(
+          Icons.star,
+          size: 18,
+          color: Colors.orange,
+        ),
+        Icon(
+          Icons.star,
+          size: 18,
+          color: Colors.orange,
+        ),
+        Icon(
+          Icons.star,
+          size: 18,
+          color: Colors.orange,
+        ),
+        Icon(
+          Icons.star,
+          size: 18,
+          color: Colors.orange,
+        ),
+        Icon(
+          Icons.star,
+          size: 18,
+          color: Colors.grey,
+        ),
+        Text("4.0"),
+      ],
+    );
+  }
+
+  Row buildData() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        Expanded(
+          flex: 4,
+          child: Text(
+            '2021-09-08 10:56',
+            style: TextStyle(color: Color(0xffaaaaaa), fontSize: 12),
+          ),
+        ),
+        Expanded(
+          flex: 1,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Icon(
+                Icons.mode_comment_outlined,
+                color: Color(0xffaaaaaa),
+                size: 16,
+              ),
+              Text(
+                '1',
+                style: TextStyle(color: Color(0xffaaaaaa), fontSize: 12),
+              ),
+              Icon(
+                Icons.thumb_up,
+                color: Color(0xffaaaaaa),
+                size: 16,
+              ),
+              Text(
+                '1',
+                style: TextStyle(color: Color(0xffaaaaaa), fontSize: 12),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+
+  Row buildCourseComment() {
+    return Row(
+      children: [
+        Expanded(
+          child: Text(
+            '多行评价文本，此处省略一万字多行评价文本，此处省略一万字多行评价文本，此处省略一万字多行评价文本，此处省略一万字多行评价文本，此处省略一万字',
+            style: TextStyle(fontSize: 13),
+            textAlign: TextAlign.start,
+            // overflow: TextOverflow.ellipsis,
+            maxLines: 2,
+          ),
+        )
+      ],
+    );
+  }
+}
---
+++ lib/pages/course/course_index.dart
@@ -0,0 +1,120 @@
+import 'package:flutter/material.dart';
+import 'package:lulab/pages/widgets/video_view.dart';
+import 'course_desc.dart';
+import 'course_comment.dart';
+import 'course_recom.dart';
+import 'package:lulab/pages/widgets/hi_tab.dart';
+
+/// 课程视频播放页面
+
+class CourseIndexPage extends StatefulWidget {
+  CourseIndexPage({Key? key}) : super(key: key);
+
+  @override
+  _CourseIndexPageState createState() => _CourseIndexPageState();
+}
+
+class _CourseIndexPageState extends State<CourseIndexPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: CourseIndex(),
+    );
+  }
+}
+
+class CourseIndex extends StatefulWidget {
+  CourseIndex({Key? key}) : super(key: key);
+
+  @override
+  _CourseIndexState createState() => _CourseIndexState();
+}
+
+class _CourseIndexState extends State<CourseIndex>
+    with TickerProviderStateMixin {
+  // 声明tabcontroller和tab标题
+  late TabController _tabController;
+  List tabs = ["简介", "评价", "推荐"];
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: tabs.length, vsync: this);
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _tabController.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        children: [
+          VideoView(
+            'http://vfx.mtime.cn/Video/2019/03/19/mp4/190319212559089721.mp4',
+            cover: 'https://images8.alphacoders.com/498/thumb-1920-498307.jpg',
+          ),
+          // tab栏
+          _buildTabNavigation(),
+          Flexible(
+            child: Padding(
+              padding: EdgeInsets.symmetric(horizontal: 10),
+              child: TabBarView(
+                controller: _tabController,
+                children: [
+                  CourseDescPage(),
+                  CourseCommentPage(),
+                  CourseRecomPage(),
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+// tab栏具体实现
+  _buildTabNavigation() {
+    //使用Material实现阴影效果
+    return Material(
+      elevation: 5,
+      shadowColor: Colors.grey[100],
+      child: Container(
+        alignment: Alignment.centerLeft,
+        padding: EdgeInsets.only(left: 20),
+        height: 39,
+        color: Colors.white,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            _tabBar(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  _tabBar() {
+    return HiTab(
+      tabs.map<Tab>((name) {
+        return Tab(
+          text: name,
+        );
+      }).toList(),
+      controller: _tabController,
+    );
+  }
+}
+
+// 底部弹起新页面
+void bounceBottomSheet(BuildContext context, Widget bouncePage) {
+  showBottomSheet(
+      context: context,
+      builder: (BuildContext context) {
+        return bouncePage;
+      });
+}
