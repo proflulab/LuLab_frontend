587f1cbc65845a7ab82585a1df33493bd3c43338
shiming.y@qq.com <shiming.y@qq.com>
2022-01-11 01:40:59 +0800

    修正

--- lib/graphql/graphql.dart
+++ lib/graphql/graphql.dart
@@ -1,7 +1,8 @@
 library schema;
 
 export 'course_content.dart';
-export 'user_login.dart';
 export 'user_register.dart';
-export 'user_sourse.dart';
+export 'user_login.dart';
 export 'user_update.dart';
+export 'user_sourse.dart';
+export 'voice.dart';
