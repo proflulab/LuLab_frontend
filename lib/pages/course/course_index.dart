4e3a4edeb7376881b688ea38aa184f6ad75458a0
杨仕明 <shiming.y@qq.com>
2022-02-23 14:59:12 +0800

    分支shiming_c

--- lib/pages/course/course_index.dart
+++ lib/pages/course/course_index.dart
@@ -4,6 +4,7 @@ import 'package:proflu/common/api/gql_latestdetailcourse.dart';
 import 'package:proflu/common/entitys/detail_coursedata.dart';
 
 import '../../common/widget/widgets.dart';
+import '../../common/utils/utils.dart';
 import 'course_comment.dart';
 import 'course_detail.dart';
 
@@ -36,10 +37,6 @@ class _CourseIndexPageState extends State<CourseIndexPage>
 
   // 读取所有课程数据
   _handleCourse() async {
-    print('=====================================================39');
-    // print(widget.product.id);
-    print(widget.product.id);
-    // print(widget.product.id ?? widget.product.courseId);
     CourseRequest variables = CourseRequest(
         dirId: widget.product.id, courseId: widget.product.firstCourseId);
     _detailCourse = await GqlDetailCourseAPI.indexPageInfo(
@@ -74,14 +71,161 @@ class _CourseIndexPageState extends State<CourseIndexPage>
             cover: 'https://images8.alphacoders.com/498/thumb-1920-498307.jpg',
           ),
           // tab栏
-          _buildTabNavigation(),
+          //_buildTabNavigation(),
           Flexible(
             child: Padding(
               padding: const EdgeInsets.symmetric(horizontal: 0),
               child: TabBarView(
                 controller: _tabController,
                 children: [
-                  CourseDetail(product: _focusData),
+                  Column(
+                    children: [
+                      const Divider(
+                        color: Color(0xffe4e4e4),
+                      ),
+                      Container(
+                        margin: const EdgeInsets.only(left: 20),
+                        child: Row(
+                          children: [
+                            const Divider(
+                              color: Color(0xffe4e4e4),
+                            ),
+                            Text(
+                              '课程名称:' + widget.product.title,
+                              textAlign: TextAlign.center,
+                              style: const TextStyle(
+                                  fontFamily: 'MyFontStyle',
+                                  fontSize: 25,
+                                  fontWeight: FontWeight.w600),
+                            ),
+                          ],
+                        ),
+                      ),
+                      ListTile(
+                        leading: const CircleAvatar(
+                          backgroundImage: AssetImage('assets/images/logo.png'),
+                        ),
+                        title: Text(
+                          '作者：' + widget.product.author,
+                          style: const TextStyle(
+                              fontFamily: 'MyFontStyle',
+                              fontSize: 16,
+                              fontWeight: FontWeight.w600),
+                        ),
+                        subtitle: Text(
+                          widget.product.authorTags,
+                          style:
+                              const TextStyle(fontSize: 16, color: Colors.grey),
+                        ),
+                      ),
+                      ListTile(
+                        leading: Text(
+                          widget.product.classTags,
+                          style: const TextStyle(
+                            color: Colors.grey,
+                          ),
+                        ),
+                        title: Row(
+                          children: [
+                            Container(
+                              margin: const EdgeInsets.symmetric(horizontal: 5),
+                              child: const Text(
+                                '颠覆式创新',
+                                style: TextStyle(color: Colors.grey),
+                              ),
+                            ),
+                            Container(
+                              margin: const EdgeInsets.symmetric(horizontal: 5),
+                              child: const Text(
+                                '公司',
+                                style: TextStyle(color: Colors.grey),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Container(
+                        margin: const EdgeInsets.symmetric(horizontal: 20),
+                        child: Row(
+                          children: const [
+                            Text(
+                              '课程介绍',
+                              style: TextStyle(
+                                  fontSize: 16, fontWeight: FontWeight.w700),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Container(
+                        margin: const EdgeInsets.all(20),
+                        child: Wrap(
+                          children: [Text(widget.product.description)],
+                        ),
+                      ),
+                      const Text(
+                        '课程目录',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                            color: Colors.green,
+                            fontFamily: 'MyFontStyle',
+                            fontSize: 20,
+                            fontWeight: FontWeight.w500),
+                      ),
+                      Container(
+                        height: 100.h,
+                        width: 500.w,
+                        child: ListView.builder(
+                          itemCount: 1,
+                          itemBuilder: (BuildContext context, int index) {
+                            return Text("data");
+                          },
+                        ),
+                      )
+                      // Container(
+                      //   height: 800.h,
+                      //   width: 500.w,
+                      //   child: ListView.builder(
+                      //     itemCount: widget.product.subCourses.length,
+                      //     itemBuilder: (context, index) {
+                      //       return InkWell(
+                      //         onTap: () async {
+                      //           Navigator.push(
+                      //               context,
+                      //               MaterialPageRoute(
+                      //                   builder: (context) => CourseIndexPage(
+                      //                       product: widget
+                      //                           .product.subCourses[index])));
+                      //         },
+                      //         child: Container(
+                      //           height: 150.h,
+                      //           decoration: const BoxDecoration(
+                      //             color: Colors.white,
+                      //             borderRadius:
+                      //                 BorderRadius.all(Radius.circular(12.0)),
+                      //             //border: Border.all(color: Colors.black54),
+                      //           ),
+                      //           margin: const EdgeInsets.all(5.0),
+                      //           child: Stack(
+                      //             children: <Widget>[
+                      //               // 课程标题
+                      //               positionedText(
+                      //                   context: context,
+                      //                   top: 30,
+                      //                   left: 190,
+                      //                   height: 40,
+                      //                   width: 200,
+                      //                   text: widget.product.subCourses[index]
+                      //                       .subTitle),
+                      //             ],
+                      //           ),
+                      //         ),
+                      //       );
+                      //     },
+                      //   ),
+                      // ),
+                    ],
+                  ),
+                  //CourseDetail(product: _focusData),
                   CourseCommentPage(product: widget.product),
                   // const CourseCatalogue(),
                   // const CourseRecomPage(),
--- lib/pages/home/home_product.dart
+++ lib/pages/home/home_product.dart
@@ -1,75 +1,83 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_echart/flutter_echart.dart';
-import 'package:zefyrka/zefyrka.dart';
+//import 'package:flutter_echart/flutter_echart.dart';
+//import 'package:zefyrka/zefyrka.dart';
 
 class Train extends StatefulWidget {
-  Train({Key? key}) : super(key: key);
+  const Train({Key? key}) : super(key: key);
 
   @override
   _TrainState createState() => _TrainState();
 }
 
 class _TrainState extends State<Train> {
-  final List<EChartPieBean> _dataList = [
-    EChartPieBean(title: "生活费", number: 200, color: Colors.lightBlueAccent),
-    EChartPieBean(title: "游玩费", number: 200, color: Colors.deepOrangeAccent),
-    EChartPieBean(title: "交通费", number: 400, color: Colors.green),
-    EChartPieBean(title: "贷款费", number: 300, color: Colors.amber),
-    EChartPieBean(title: "电话费", number: 200, color: Colors.orange),
-  ];
-  ZefyrController _controller = ZefyrController();
   @override
   Widget build(BuildContext context) {
-    return buildPieChatWidget(_dataList);
-    // return const Text('nihapo');
-    // return Container(
-    //     width: 420.0, //容器的相关参数
-    //     height: 245.0,
-    //     alignment: Alignment.center, //在容器的位置
-    //     child: buildPieChatWidget(_dataList));
-    // return ChartTestPage();
-    // return SearchBarDemo();
-    // return MyHomePage(
-    //   title: '',
-    // );
-    // return FanYe();
-    // return Column(
-    //   children: [
-    //     ZefyrToolbar.basic(controller: _controller),
-    //     Expanded(
-    //       child: ZefyrEditor(
-    //         controller: _controller,
-    //       ),
-    //     ),
-    //   ],
-    // );
-    // return TanChuang();
-    // return UserSet();
-  }
-
-  PieChatWidget buildPieChatWidget(_dataList) {
-    return PieChatWidget(
-      dataList: _dataList,
-      //是否输出日志
-      isLog: true,
-      //是否需要背景
-      isBackground: true,
-      //是否画直线
-      isLineText: true,
-      //背景
-      bgColor: Colors.white,
-      //是否显示最前面的内容
-      isFrontgText: true,
-      //默认选择放大的块
-      initSelect: 1,
-      //初次显示以动画方式展开
-      openType: OpenType.ANI,
-      //旋转类型
-      loopType: LoopType.DOWN_LOOP,
-      //点击回调
-      clickCallBack: (int value) {
-        print("当前点击显示 $value");
-      },
-    );
+    return const Text("data");
   }
 }
+
+
+// class _TrainState extends State<Train> {
+//   final List<EChartPieBean> _dataList = [
+//     EChartPieBean(title: "生活费", number: 200, color: Colors.lightBlueAccent),
+//     EChartPieBean(title: "游玩费", number: 200, color: Colors.deepOrangeAccent),
+//     EChartPieBean(title: "交通费", number: 400, color: Colors.green),
+//     EChartPieBean(title: "贷款费", number: 300, color: Colors.amber),
+//     EChartPieBean(title: "电话费", number: 200, color: Colors.orange),
+//   ];
+//   //ZefyrController _controller = ZefyrController();
+//   @override
+//   Widget build(BuildContext context) {
+//     return buildPieChatWidget(_dataList);
+//     // return const Text('nihapo');
+//     // return Container(
+//     //     width: 420.0, //容器的相关参数
+//     //     height: 245.0,
+//     //     alignment: Alignment.center, //在容器的位置
+//     //     child: buildPieChatWidget(_dataList));
+//     // return ChartTestPage();
+//     // return SearchBarDemo();
+//     // return MyHomePage(
+//     //   title: '',
+//     // );
+//     // return FanYe();
+//     // return Column(
+//     //   children: [
+//     //     ZefyrToolbar.basic(controller: _controller),
+//     //     Expanded(
+//     //       child: ZefyrEditor(
+//     //         controller: _controller,
+//     //       ),
+//     //     ),
+//     //   ],
+//     // );
+//     // return TanChuang();
+//     // return UserSet();
+//   }
+
+//   PieChatWidget buildPieChatWidget(_dataList) {
+//     return PieChatWidget(
+//       dataList: _dataList,
+//       //是否输出日志
+//       isLog: true,
+//       //是否需要背景
+//       isBackground: true,
+//       //是否画直线
+//       isLineText: true,
+//       //背景
+//       bgColor: Colors.white,
+//       //是否显示最前面的内容
+//       isFrontgText: true,
+//       //默认选择放大的块
+//       initSelect: 1,
+//       //初次显示以动画方式展开
+//       openType: OpenType.ANI,
+//       //旋转类型
+//       loopType: LoopType.DOWN_LOOP,
+//       //点击回调
+//       clickCallBack: (int value) {
+//         print("当前点击显示 $value");
+//       },
+//     );
+//   }
+// }
