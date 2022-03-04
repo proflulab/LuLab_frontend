82a2ff0fe3fcee93c526d59adb22bd41f50c4b9c
杨仕明 <shiming.y@qq.com>
2022-03-04 21:28:11 +0800

    完善课程页面

---
+++ lib/common/widget/fijkplayer_video.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:fijkplayer/fijkplayer.dart';
+
+class VideoScreen extends StatefulWidget {
+  final String url;
+  final String cover;
+  const VideoScreen({Key? key, required this.url, required this.cover})
+      : super(key: key);
+
+  @override
+  State<VideoScreen> createState() => _VideoScreenState();
+}
+
+class _VideoScreenState extends State<VideoScreen> {
+  final FijkPlayer player = FijkPlayer();
+
+  _VideoScreenState();
+
+  @override
+  void initState() {
+    super.initState();
+    player.setDataSource(widget.url, autoPlay: true);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // double screenWidth = MediaQuery.of(context).size.width;
+    // double playerHeight = screenWidth / 1.5;
+    return Container(
+      // width: screenWidth,
+      // height: playerHeight,
+      // color: Colors.black,
+      height: 235,
+      alignment: Alignment.center,
+      child: FijkView(
+        player: player,
+        panelBuilder: fijkPanel2Builder(fill: true),
+        cover: NetworkImage(widget.cover),
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    player.release();
+  }
+}
--- lib/pages/course/course_index.dart
+++ lib/pages/course/course_index.dart
@@ -7,7 +7,7 @@ import '../../common/widget/widgets.dart';
 
 import 'course_comment.dart';
 
-/// 课程视频播放页面
+// 课程视频播放页面
 
 class CourseIndexPage extends StatefulWidget {
   const CourseIndexPage({Key? key, required this.product}) : super(key: key);
@@ -32,6 +32,7 @@ class _CourseIndexPageState extends State<CourseIndexPage>
   var _focusData;
 
   int _selectIndex = 0;
+  List _subCourses = [];
 
   @override
   void initState() {
@@ -45,9 +46,9 @@ class _CourseIndexPageState extends State<CourseIndexPage>
     CourseRequest variables = CourseRequest(dirId: dirId, courseId: courseId);
     _detailCourse = await GqlDetailCourseAPI.indexPageInfo(
         variables: variables, context: context);
-    var focusList = _detailCourse.detailCourse;
     setState(() {
-      _focusData = focusList;
+      _focusData = _detailCourse.detailCourse;
+      _subCourses = _detailCourse.detailCourse.subCourses;
     });
   }
 
@@ -66,173 +67,177 @@ class _CourseIndexPageState extends State<CourseIndexPage>
         toolbarOpacity: 0,
         backgroundColor: (Colors.black),
       ),
-      body: Column(
-        children: [
-          VideoView(vUrl,
-              //'https://media.w3.org/2010/05/sintel/trailer.mp4',
-              //'https://media.w3.org/2010/05/sintel/trailer.mp4',
-              //'https://flutter.github.io/assets-for-api-docs/assets/videos/butterfly.mp4',
-              cover:
-                  'https://images8.alphacoders.com/498/thumb-1920-498307.jpg',
-              key: UniqueKey()),
-          // tab栏
-          _buildTabNavigation(),
-          Flexible(
-            child: Container(
-              margin: EdgeInsets.only(left: 20.w, right: 20.w),
-              //padding: EdgeInsets.only(left: 20.w, right: 20.w),
-              child: TabBarView(
-                controller: _tabController,
-                children: [
-                  ListView(
-                    children: [
-                      const Divider(
-                        color: Color(0xffe4e4e4),
-                      ),
-                      Text(
-                        widget.product.title,
-                        textAlign: TextAlign.left,
-                        style: const TextStyle(
-                            fontFamily: 'MyFontStyle',
-                            fontSize: 25,
-                            fontWeight: FontWeight.w600),
-                      ),
-                      SizedBox(
-                        height: 10.h,
-                      ),
-                      ListTile(
-                        contentPadding: EdgeInsets.only(left: 0.w, right: 0.w),
-                        leading: CircleAvatar(
-                            backgroundImage:
-                                NetworkImage(widget.product.imgUrl)),
-                        title: Text(
-                          widget.product.author,
+      body: Container(
+        child: Column(
+          children: [
+            VideoView(vUrl,
+                //'https://media.w3.org/2010/05/sintel/trailer.mp4',
+                //'https://media.w3.org/2010/05/sintel/trailer.mp4',
+                //'https://flutter.github.io/assets-for-api-docs/assets/videos/butterfly.mp4',
+                cover:
+                    'https://images8.alphacoders.com/498/thumb-1920-498307.jpg',
+                key: UniqueKey()),
+            // tab栏
+            _buildTabNavigation(),
+            Flexible(
+              child: Container(
+                margin: EdgeInsets.only(left: 20.w, right: 20.w),
+                //padding: EdgeInsets.only(left: 20.w, right: 20.w),
+                child: TabBarView(
+                  controller: _tabController,
+                  children: [
+                    ListView(
+                      children: [
+                        const Divider(
+                          color: Color(0xffe4e4e4),
+                        ),
+                        Text(
+                          widget.product.title,
+                          textAlign: TextAlign.left,
                           style: const TextStyle(
                               fontFamily: 'MyFontStyle',
-                              fontSize: 16,
+                              fontSize: 25,
                               fontWeight: FontWeight.w600),
                         ),
-                        subtitle: Text(
-                          widget.product.authorTags,
-                          style:
-                              const TextStyle(fontSize: 16, color: Colors.grey),
+                        SizedBox(
+                          height: 10.h,
                         ),
-                      ),
-                      // ListTile(
-                      //   leading: Text(
-                      //     _focusData.classTags,
-                      //     style: const TextStyle(
-                      //       color: Colors.grey,
-                      //     ),
-                      //   ),
-                      //   title: Row(
-                      //     children: [
-                      //       Container(
-                      //         margin: const EdgeInsets.symmetric(horizontal: 5),
-                      //         child: const Text(
-                      //           '颠覆式创新',
-                      //           style: TextStyle(color: Colors.grey),
-                      //         ),
-                      //       ),
-                      //       Container(
-                      //         margin: const EdgeInsets.symmetric(horizontal: 5),
-                      //         child: const Text(
-                      //           '公司',
-                      //           style: TextStyle(color: Colors.grey),
-                      //         ),
-                      //       ),
-                      //     ],
-                      //   ),
-                      // ),
-                      // Container(
-                      //   margin: const EdgeInsets.symmetric(horizontal: 20),
-                      //   child: Row(
-                      //     children: const [
-                      //       Text(
-                      //         '课程介绍',
-                      //         style: TextStyle(fontSize: 16, fontWeight: FontWeight.w700),
-                      //       ),
-                      //     ],
-                      //   ),
-                      // ),
-                      Wrap(
-                        children: [Text(widget.product.description)],
-                      ),
-                      const Divider(
-                        color: Color(0xffe4e4e4),
-                      ),
-                      const Text(
-                        '目录',
-                        textAlign: TextAlign.left,
-                        style: TextStyle(
-                            color: Colors.green,
-                            fontFamily: 'MyFontStyle',
-                            fontSize: 25,
-                            fontWeight: FontWeight.w500),
-                      ),
-                      SizedBox(
-                        height: (114.h + 15) * _focusData.subCourses.length,
-                        width: 500.w,
-                        child: ListView.builder(
-                          physics: const NeverScrollableScrollPhysics(),
-                          itemCount: _focusData.subCourses.length,
-                          itemBuilder: (context, index) {
-                            if (_focusData.subCourses.isNotEmpty) {
-                              return InkWell(
-                                onTap: () async {
-                                  setState(() {
-                                    dirId = _focusData
-                                        .subCourses[index].mainCourseId;
-                                    courseId =
-                                        _focusData.subCourses[index].courseId;
-                                    _handleCourse();
-                                    _selectIndex = index;
-                                    vUrl = _focusData.videoUrl;
-                                  });
-                                  //_handleCourse();
-                                },
-                                child: Container(
-                                  height: 114.h,
-                                  decoration: BoxDecoration(
-                                    color: _selectIndex == index
-                                        ? Color.fromRGBO(6, 202, 124, 0.5)
-                                        : Colors.white,
-                                    borderRadius: const BorderRadius.all(
-                                        Radius.circular(12.0)),
-                                    //border: Border.all(color: Colors.black54),
-                                  ),
-                                  margin: const EdgeInsets.all(5.0),
-                                  child: Stack(
-                                    children: <Widget>[
-                                      // 课程标题
-                                      positionedText(
-                                          context: context,
-                                          top: 45,
-                                          left: 30,
-                                          height: 40,
-                                          width: 200,
-                                          text: _focusData
-                                              .subCourses[index].subTitle),
-                                    ],
-                                  ),
-                                ),
-                              );
-                            } else {
-                              return const Text('加载中...');
-                            }
-                          },
+                        ListTile(
+                          contentPadding:
+                              EdgeInsets.only(left: 0.w, right: 0.w),
+                          leading: CircleAvatar(
+                              backgroundImage:
+                                  NetworkImage(widget.product.imgUrl)),
+                          title: Text(
+                            widget.product.author,
+                            style: const TextStyle(
+                                fontFamily: 'MyFontStyle',
+                                fontSize: 16,
+                                fontWeight: FontWeight.w600),
+                          ),
+                          subtitle: Text(
+                            widget.product.authorTags,
+                            style: const TextStyle(
+                                fontSize: 16, color: Colors.grey),
+                          ),
                         ),
-                      ),
-                    ],
-                  ),
-                  CourseCommentPage(product: widget.product),
-                  // const CourseCatalogue(),
-                  // const CourseRecomPage(),
-                ],
+                        // ListTile(
+                        //   leading: Text(
+                        //     _focusData.classTags,
+                        //     style: const TextStyle(
+                        //       color: Colors.grey,
+                        //     ),
+                        //   ),
+                        //   title: Row(
+                        //     children: [
+                        //       Container(
+                        //         margin: const EdgeInsets.symmetric(horizontal: 5),
+                        //         child: const Text(
+                        //           '颠覆式创新',
+                        //           style: TextStyle(color: Colors.grey),
+                        //         ),
+                        //       ),
+                        //       Container(
+                        //         margin: const EdgeInsets.symmetric(horizontal: 5),
+                        //         child: const Text(
+                        //           '公司',
+                        //           style: TextStyle(color: Colors.grey),
+                        //         ),
+                        //       ),
+                        //     ],
+                        //   ),
+                        // ),
+                        // Container(
+                        //   margin: const EdgeInsets.symmetric(horizontal: 20),
+                        //   child: Row(
+                        //     children: const [
+                        //       Text(
+                        //         '课程介绍',
+                        //         style: TextStyle(fontSize: 16, fontWeight: FontWeight.w700),
+                        //       ),
+                        //     ],
+                        //   ),
+                        // ),
+                        Wrap(
+                          children: [Text(widget.product.description)],
+                        ),
+                        const Divider(
+                          color: Color(0xffe4e4e4),
+                        ),
+                        const Text(
+                          '目录',
+                          textAlign: TextAlign.left,
+                          style: TextStyle(
+                              color: Colors.green,
+                              fontFamily: 'MyFontStyle',
+                              fontSize: 25,
+                              fontWeight: FontWeight.w500),
+                        ),
+                        SizedBox(
+                          height: (114.h + 15) * _subCourses.length,
+                          width: 500.w,
+                          child: ListView.builder(
+                            physics: const NeverScrollableScrollPhysics(),
+                            itemCount: _subCourses.length,
+                            itemBuilder: (context, index) {
+                              if (_subCourses.length != null) {
+                                return InkWell(
+                                  onTap: () async {
+                                    setState(() {
+                                      dirId = _focusData
+                                          .subCourses[index].mainCourseId;
+                                      courseId =
+                                          _focusData.subCourses[index].courseId;
+                                      _handleCourse();
+                                      _selectIndex = index;
+                                      vUrl = _focusData.videoUrl;
+                                    });
+                                    //_handleCourse();
+                                  },
+                                  child: Container(
+                                    height: 114.h,
+                                    decoration: BoxDecoration(
+                                      color: _selectIndex == index
+                                          ? const Color.fromRGBO(
+                                              6, 202, 124, 0.5)
+                                          : Colors.white,
+                                      borderRadius: const BorderRadius.all(
+                                          Radius.circular(12.0)),
+                                      //border: Border.all(color: Colors.black54),
+                                    ),
+                                    margin: const EdgeInsets.all(5.0),
+                                    child: Stack(
+                                      children: <Widget>[
+                                        // 课程标题
+                                        positionedText(
+                                            context: context,
+                                            top: 45,
+                                            left: 30,
+                                            height: 40,
+                                            width: 200,
+                                            text: _focusData
+                                                .subCourses[index].subTitle),
+                                      ],
+                                    ),
+                                  ),
+                                );
+                              } else {
+                                return const Text('加载中...');
+                              }
+                            },
+                          ),
+                        ),
+                      ],
+                    ),
+                    CourseCommentPage(product: widget.product),
+                    // const CourseCatalogue(),
+                    // const CourseRecomPage(),
+                  ],
+                ),
               ),
-            ),
-          )
-        ],
+            )
+          ],
+        ),
       ),
     );
   }
