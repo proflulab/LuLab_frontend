51c8549e44ba55ef86de2649b381f6f6afc1bc59
杨仕明 <shiming.y@qq.com>
2022-03-11 15:04:15 +0800

    course_page页面迭代

--- lib/pages/course/course_page.dart
+++ lib/pages/course/course_page.dart
@@ -21,6 +21,7 @@ class _CoursePageState extends State<CoursePage> {
   int _selectIndex = 0;
 
   final double _selectColumnsWidth = 0.245333333333333333.sw;
+
   final List _courseid = ['理论课程', '大咖访谈', '举例1', '举例2', '举例3', '举例4'];
 
   @override
@@ -103,138 +104,153 @@ class _CoursePageState extends State<CoursePage> {
         child: Row(
           children: <Widget>[
             SizedBox(
-                width: _selectColumnsWidth,
-                height: double.infinity,
-                child: Column(
-                  children: [
-                    Expanded(
-                      flex: 1,
-                      child: ListView.builder(
-                        itemCount: _courseid.length,
-                        itemBuilder: (context, index) {
-                          return InkWell(
-                            onTap: () {
-                              setState(() {
-                                if (kDebugMode) {
-                                  print(DateTime.now());
-                                  print("点击了课程目录");
-                                  // print(DateTime.now().millisecondsSinceEpoch);
-                                  // print(DateTime.fromMillisecondsSinceEpoch(
-                                  //     DateTime.now().millisecondsSinceEpoch));
-                                }
-                                _selectIndex = index;
-                              });
-                            },
-                            child: Container(
-                              width: double.infinity,
-                              height: _selectColumnsWidth * 0.618,
-                              decoration: BoxDecoration(
-                                  color: _selectIndex == index
-                                      ? Colors.white
-                                      : const Color.fromRGBO(246, 246, 246, 1),
-                                  borderRadius: _selectIndex == index
-                                      ? const BorderRadius.only()
-                                      : _selectIndex == index + 1
-                                          ? const BorderRadius.only(
-                                              bottomRight: Radius.circular(10),
-                                            )
-                                          : _selectIndex == index - 1
-                                              ? const BorderRadius.only(
-                                                  topRight: Radius.circular(10))
-                                              : const BorderRadius.only()),
-                              child: Center(
-                                child: Text(_courseid[index],
-                                    style: _selectIndex == index
-                                        ? const TextStyle(
-                                            fontFamily: 'MyFontStyle',
-                                            color:
-                                                Color.fromRGBO(51, 51, 51, 1),
-                                            fontSize: 20,
-                                          )
-                                        : const TextStyle(
-                                            color: Color.fromRGBO(
-                                                136, 136, 136, 1),
-                                            fontSize: 18,
-                                          ),
-                                    textAlign: TextAlign.center),
-                              ),
-                            ),
-                          );
-                        },
-                      ),
-                    ),
-                    Container(
-                      color: const Color.fromRGBO(246, 246, 246, 1),
-                      width: _selectColumnsWidth,
-                      height: 1.sh -
-                          (_selectColumnsWidth * 0.618 * _courseid.length +
-                              220.h),
-                    ),
-                  ],
-                )),
-            Expanded(
-                flex: 1,
-                child: Container(
-                  decoration: const BoxDecoration(color: Colors.white),
-                  padding: const EdgeInsets.only(left: 10, right: 12),
-                  child: ListView.builder(
-                    physics: const NeverScrollableScrollPhysics(),
-                    itemCount: _focusData.length,
-                    itemBuilder: (context, index) {
-                      if (_focusData.isNotEmpty) {
+              width: _selectColumnsWidth,
+              child: Column(
+                children: [
+                  SizedBox(
+                    height: _courseid.length * _selectColumnsWidth * 0.618,
+                    child: ListView.builder(
+                      physics: const NeverScrollableScrollPhysics(),
+                      itemCount: _courseid.length,
+                      itemBuilder: (context, index) {
                         return InkWell(
-                          onTap: () async {
-                            Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                    builder: (context) => CourseIndexPage(
-                                        product: _focusData[index])));
+                          onTap: () {
+                            setState(() {
+                              if (kDebugMode) {
+                                print(DateTime.now());
+                                print('点击了' + _courseid[index]);
+                                // print(DateTime.now().millisecondsSinceEpoch);
+                                // print(DateTime.fromMillisecondsSinceEpoch(
+                                //     DateTime.now().millisecondsSinceEpoch));
+                              }
+                              _selectIndex = index;
+                            });
                           },
                           child: Container(
-                            height: 170.h,
-                            decoration: const BoxDecoration(
-                              color: Colors.white,
-                              borderRadius:
-                                  BorderRadius.all(Radius.circular(12.0)),
-                              //border: Border.all(color: Colors.black54),
-                            ),
-                            margin: const EdgeInsets.only(bottom: 10),
-                            child: Stack(
-                              children: <Widget>[
-                                // 课程封面
-                                positionedImage(
-                                    context: context,
-                                    top: 10,
-                                    left: 10,
-                                    height: 150,
-                                    width: 160,
-                                    url: _focusData[index].imgUrl),
-                                // 课程标题
-                                positionedText(
-                                    context: context,
-                                    top: 30,
-                                    left: 190,
-                                    height: 80.h,
-                                    width: 700.w,
-                                    text: _focusData[index].title),
-                                // 作者
-                                positionedText(
-                                    context: context,
-                                    top: 55,
-                                    left: 190,
-                                    height: 80.h,
-                                    width: 700.w,
-                                    text: _focusData[index].author),
-                              ],
+                            width: double.infinity,
+                            height: _selectColumnsWidth * 0.618,
+                            decoration: BoxDecoration(
+                                color: _selectIndex == index
+                                    ? Colors.white
+                                    : const Color.fromRGBO(246, 246, 246, 1),
+                                borderRadius: _selectIndex == index
+                                    ? null
+                                    : _selectIndex == index + 1
+                                        ? const BorderRadius.only(
+                                            bottomRight: Radius.circular(10),
+                                          )
+                                        : _selectIndex == index - 1
+                                            ? const BorderRadius.only(
+                                                topRight: Radius.circular(10))
+                                            : null),
+                            child: Center(
+                              child: Text(_courseid[index],
+                                  style: _selectIndex == index
+                                      ? const TextStyle(
+                                          fontFamily: 'MyFontStyle',
+                                          color: Color.fromRGBO(51, 51, 51, 1),
+                                          fontSize: 20,
+                                        )
+                                      : const TextStyle(
+                                          color:
+                                              Color.fromRGBO(136, 136, 136, 1),
+                                          fontSize: 18,
+                                        ),
+                                  textAlign: TextAlign.center),
                             ),
                           ),
                         );
-                      } else {
-                        return const Text('加载中...');
-                      }
-                    },
+                      },
+                    ),
                   ),
-                ))
+                  SizedBox(
+                    height: _selectColumnsWidth * 0.618,
+                    child: ListView.builder(
+                      physics: const NeverScrollableScrollPhysics(),
+                      itemCount: 1,
+                      itemBuilder: (context, index) {
+                        return Container(
+                          width: double.infinity,
+                          height: _selectColumnsWidth * 0.618,
+                          decoration: BoxDecoration(
+                              color: const Color.fromRGBO(246, 246, 246, 1),
+                              borderRadius: _selectIndex == _courseid.length - 1
+                                  ? const BorderRadius.only(
+                                      topRight: Radius.circular(10))
+                                  : null),
+                        );
+                      },
+                    ),
+                  ),
+                  Flexible(
+                      child: Container(
+                    color: const Color.fromRGBO(246, 246, 246, 1),
+                    width: _selectColumnsWidth,
+                  )),
+                ],
+              ),
+            ),
+            Flexible(
+                child: Container(
+              decoration: const BoxDecoration(color: Colors.white),
+              padding: const EdgeInsets.only(left: 10, right: 12),
+              child: ListView.builder(
+                physics: const NeverScrollableScrollPhysics(),
+                itemCount: _focusData.length,
+                itemBuilder: (context, index) {
+                  if (_focusData.isNotEmpty) {
+                    return InkWell(
+                      onTap: () async {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => CourseIndexPage(
+                                    product: _focusData[index])));
+                      },
+                      child: Container(
+                        height: 170.h,
+                        decoration: const BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.all(Radius.circular(12.0)),
+                          //border: Border.all(color: Colors.black54),
+                        ),
+                        margin: const EdgeInsets.only(bottom: 10),
+                        child: Stack(
+                          children: <Widget>[
+                            // 课程封面
+                            positionedImage(
+                                context: context,
+                                top: 10,
+                                left: 10,
+                                height: 150,
+                                width: 160,
+                                url: _focusData[index].imgUrl),
+                            // 课程标题
+                            positionedText(
+                                context: context,
+                                top: 30,
+                                left: 190,
+                                height: 80.h,
+                                width: 700.w,
+                                text: _focusData[index].title),
+                            // 作者
+                            positionedText(
+                                context: context,
+                                top: 55,
+                                left: 190,
+                                height: 80.h,
+                                width: 700.w,
+                                text: _focusData[index].author),
+                          ],
+                        ),
+                      ),
+                    );
+                  } else {
+                    return const Text('加载中...');
+                  }
+                },
+              ),
+            ))
           ],
         ),
       ),
