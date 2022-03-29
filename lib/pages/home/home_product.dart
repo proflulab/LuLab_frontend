f25b577c0de82fab5f4222c57fc51ee113f0f3ef
Your Name <email@example.com>
2022-03-29 20:38:53 +0800

    训练营

--- lib/pages/home/home_product.dart
+++ lib/pages/home/home_product.dart
@@ -29,117 +29,137 @@ class _ProductState extends State<Product> {
       itemBuilder: (BuildContext context, int index) {
         return GestureDetector(
           child: Container(
-              margin: const EdgeInsets.all(10),
-              alignment: Alignment.topLeft,
-              width: 1.sw,
-              height: 200,
-              color: Colors.white,
-              child: Column(
-                children: [
-                  Container(
-                    width: 1.sw,
-                    height: 180,
-                    child: Column(
-                      children: [
-                        const SizedBox(height: 10),
-                        Container(
-                          width: 340,
-                          height: 100,
+            margin: const EdgeInsets.all(10),
+            alignment: Alignment.topLeft,
+            width: 1.sw,
+            height: 200,
+            child: Column(
+              children: [
+                Container(
+                  width: 1.sw,
+                  height: 180,
+                  child: Column(
+                    children: [
+                      const SizedBox(height: 10),
+                      Container(
+                        width: 340,
+                        height: 100,
 
-                          child: Row(
-                            children: [
-                              Expanded(
-                                  flex: 1,
-                                  child: Container(
-                                    alignment: Alignment.center,
-                                    height: 100,
-                                    child: Text(
-                                      _postsDataProduct
-                                          .data.product[index].name,
-                                    ),
-                                  )),
-                              Expanded(
-                                  flex: 1,
-                                  child: Container(
-                                    decoration: BoxDecoration(
-                                      image: DecorationImage(
-                                        image: NetworkImage(
-                                          _postsDataProduct
-                                              .data.product[index].img,
-                                        ), // 图片数组
-                                        fit: BoxFit.cover,
-                                      ),
-                                      borderRadius: const BorderRadius.only(
-                                        topRight: Radius.circular(10),
+                        child: Row(
+                          children: [
+                            Expanded(
+                                flex: 1,
+                                child: Column(
+                                  children: [
+                                    Container(
+                                      alignment: Alignment.center,
+                                      height: 50,
+                                      child: Text(
+                                        _postsDataProduct
+                                            .data.product[index].name,
                                       ),
                                     ),
-                                  )),
-                            ],
-                          ),
-                          // Image.network(
-                          //   'https://img2.baidu.com/it/u=295131672,1212858880&fm=253&fmt=auto&app=138&f=JPEG?w=325&h=200',
-                          //   fit: BoxFit.fill,
-                          //   width: 400,
-                          // ),
-                          decoration: const BoxDecoration(
-                            borderRadius: BorderRadius.only(
-                                topLeft: Radius.circular(10),
-                                topRight: Radius.circular(10)),
-                            color: Colors.red,
-                          ),
-                        ),
-                        Container(
-                          margin: const EdgeInsets.all(10.0),
-                          child: Row(
-                            children: <Widget>[
-                              Expanded(
+                                    Container(
+                                      alignment: Alignment.center,
+                                      height: 50,
+                                      child: Text(
+                                        _postsDataProduct
+                                            .data.product[index].description,
+                                      ),
+                                    )
+                                  ],
+                                )),
+                            Expanded(
                                 flex: 1,
                                 child: Container(
-                                  alignment: Alignment.center,
-                                  height: 50,
-                                  child: const Text('特点1'),
-                                ),
-                              ),
-                              Container(
-                                width: 1,
-                                color: Colors.black,
-                              ),
-                              Expanded(
-                                  flex: 1,
-                                  child: SizedBox(
-                                      height: 50,
-                                      child: Container(
-                                        alignment: Alignment.center,
-                                        height: 50,
-                                        child: const Text(' 特点2'),
-                                      ))),
-                              Container(
-                                width: 1,
-                                color: Colors.black,
-                              ),
-                              Expanded(
-                                  flex: 1,
-                                  child: SizedBox(
-                                      height: 50,
-                                      child: Container(
-                                        alignment: Alignment.center,
-                                        height: 50,
-                                        child: const Text(' 特点3'),
-                                      ))),
-                            ],
-                          ),
+                                  decoration: BoxDecoration(
+                                    image: DecorationImage(
+                                      image: NetworkImage(
+                                        _postsDataProduct
+                                            .data.product[index].img,
+                                      ), // 图片数组
+                                      fit: BoxFit.cover,
+                                    ),
+                                    borderRadius: const BorderRadius.only(
+                                      topRight: Radius.circular(10),
+                                    ),
+                                  ),
+                                )),
+                          ],
                         ),
-                      ],
-                    ),
-                    //Image.network(
-                    //   _postsDataProduct.data.product[index].img,
-                    //   fit: BoxFit.contain,
-                    //   height: 100,
-                    //   width: 100,
-                    // ),
+                        // Image.network(
+                        //   'https://img2.baidu.com/it/u=295131672,1212858880&fm=253&fmt=auto&app=138&f=JPEG?w=325&h=200',
+                        //   fit: BoxFit.fill,
+                        //   width: 400,
+                        // ),
+                        decoration: const BoxDecoration(
+                          borderRadius: BorderRadius.only(
+                              topLeft: Radius.circular(10),
+                              topRight: Radius.circular(10)),
+                          color: Colors.red,
+                        ),
+                      ),
+                      Container(
+                        margin: const EdgeInsets.all(10.0),
+                        child: Row(
+                          children: <Widget>[
+                            Expanded(
+                              flex: 1,
+                              child: Container(
+                                alignment: Alignment.center,
+                                height: 50,
+                                child: const Text('特点1'),
+                              ),
+                            ),
+                            Container(
+                              width: 1,
+                              color: Colors.black,
+                            ),
+                            Expanded(
+                                flex: 1,
+                                child: SizedBox(
+                                    height: 50,
+                                    child: Container(
+                                      alignment: Alignment.center,
+                                      height: 50,
+                                      child: const Text(' 特点2'),
+                                    ))),
+                            Container(
+                              width: 1,
+                              color: Colors.black,
+                            ),
+                            Expanded(
+                                flex: 1,
+                                child: SizedBox(
+                                    height: 50,
+                                    child: Container(
+                                      alignment: Alignment.center,
+                                      height: 50,
+                                      child: const Text(' 特点3'),
+                                    ))),
+                          ],
+                        ),
+                      ),
+                    ],
                   ),
-                ],
-              )),
+                  //Image.network(
+                  //   _postsDataProduct.data.product[index].img,
+                  //   fit: BoxFit.contain,
+                  //   height: 100,
+                  //   width: 100,
+                  // ),
+                ),
+              ],
+            ),
+            decoration: BoxDecoration(
+//背景
+              color: Colors.white,
+              //设置四周圆角 角度
+              borderRadius: const BorderRadius.all(Radius.circular(10.0)),
+              //设置四周边框
+              border: Border.all(width: 1, color: Colors.white),
+            ),
+          ),
           onTap: () {
             Navigator.push(
               context,
