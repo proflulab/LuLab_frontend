cada1ec833f755eea72e32ecccc32ef8e2306e1b
17610608923 <1223517276@qq.com>
2022-04-29 13:56:50 +0800

    通知页面缺省

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -1,9 +1,10 @@
+import 'package:blur/blur.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import '../../common/values/values.dart';
 import 'home_feats.dart';
 import 'home_gather.dart';
-import 'home_product.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -26,42 +27,50 @@ class _HomePageState extends State<HomePage>
         appBar: PreferredSize(
           //设置appBar高度
           preferredSize: const Size.fromHeight(60),
-          child: AppBar(
-            automaticallyImplyLeading: false,
-            bottom: const TabBar(
-              tabs: [
-                Tab(text: "推荐区"),
-                Tab(text: "功勋园"),
-                //TODO 训练营产品取消，后续修改为会员申请入口
-                //Tab(text: "训练营"),
-              ],
-              //文字选中颜色
-              labelColor: PFc.themeColor,
-              //文字未选中颜色
-              unselectedLabelColor: PFc.textPrimary,
-              //文字选中样式
-              labelStyle: TextStyle(
-                fontWeight: FontWeight.bold,
-                fontFamily: 'MyFontStyle',
-                fontSize: 20,
+          child: Blur(
+            blur: 10,
+            // blurColor: Colors.red,
+            colorOpacity: 0.1,
+            child: Container(color: Colors.transparent),
+            overlay: AppBar(
+              automaticallyImplyLeading: false,
+              backgroundColor: Colors.transparent,
+              systemOverlayStyle: SystemUiOverlayStyle.dark,
+              bottom: const TabBar(
+                tabs: [
+                  Tab(text: "推荐区"),
+                  Tab(text: "功勋园"),
+                  //TODO 训练营产品取消，后续修改为会员申请入口
+                  //Tab(text: "训练营"),
+                ],
+                //文字选中颜色
+                labelColor: PFc.themeColor,
+                //文字未选中颜色
+                unselectedLabelColor: PFc.textPrimary,
+                //文字选中样式
+                labelStyle: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontFamily: 'MyFontStyle',
+                  fontSize: 20,
+                ),
+                //文字未选中样式
+                unselectedLabelStyle: TextStyle(
+                  fontWeight: FontWeight.normal,
+                  //fontFamily: 'MyFontStyle2',
+                  fontSize: 15,
+                ),
+                //设置线条的颜色
+                indicatorColor: PFc.themeColor,
+                //设置线条的粗细
+                indicatorWeight: 4,
+                indicatorPadding: EdgeInsets.only(
+                  right: 40,
+                  left: 40,
+                  bottom: 3,
+                ),
               ),
-              //文字未选中样式
-              unselectedLabelStyle: TextStyle(
-                fontWeight: FontWeight.normal,
-                //fontFamily: 'MyFontStyle2',
-                fontSize: 15,
-              ),
-              //设置线条的颜色
-              indicatorColor: PFc.themeColor,
-              //设置线条的粗细
-              indicatorWeight: 4,
-              indicatorPadding: EdgeInsets.only(
-                right: 40,
-                left: 40,
-                bottom: 3,
-              ),
+              elevation: 0,
             ),
-            elevation: 0,
           ),
         ),
       ),
--- lib/pages/users/notice_page.dart
+++ lib/pages/users/notice_page.dart
@@ -1,7 +1,9 @@
 import 'package:date_format/date_format.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:proflu/common/values/values.dart';
 import 'package:proflu/common/widget/my_appbar.dart';
+import 'package:proflu/common/widget/widgets.dart';
 
 import '../../common/api/apis.dart';
 import '../../common/entitys/entitys.dart';
@@ -18,7 +20,7 @@ class NoticePage extends StatefulWidget {
 
 class _NoticePageState extends State<NoticePage> {
   late LatestRecord _latestRecord;
-  List<LatestRecordElement> _focusData = [];
+  List<LatestRecordElement>? _focusData;
   @override
   void initState() {
     super.initState();
@@ -48,123 +50,139 @@ class _NoticePageState extends State<NoticePage> {
         // decoration: BoxDecoration(
         //     color: Colors.black12,
         //     ),
-        child: ListView.builder(
-          itemCount: _focusData.length,
-          itemBuilder: (contxt, index) {
-            var future = DateTime.fromMillisecondsSinceEpoch(
-                _focusData[index].onlineTime);
-            String time = formatDate(future, [mm, '月', dd, '日']);
-            if (_focusData.isNotEmpty) {
-              return InkWell(
-                onTap: () async {
-                  if (kDebugMode) {
-                    print('到通知详情');
-                  }
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) =>
-                          NoticeDetail(product: _focusData[index]),
-                    ),
-                  );
-                },
-                child: Column(
-                  children: [
-                    Container(
-                      height: 150.h,
-                      width: 1.sw,
-                      decoration: const BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.all(
-                          Radius.circular(30),
+        child: _buildBody(context),
+      ),
+    );
+  }
+
+  Widget _buildBody(BuildContext context) {
+    if (_focusData == null) {
+      return Container(
+        width: double.infinity,
+        height: double.infinity,
+        decoration: const BoxDecoration(color: Colors.transparent),
+        alignment: Alignment.center,
+        child: Container(
+          height: 80,
+          padding: const EdgeInsets.all(10),
+          decoration: BoxDecoration(
+              color: const Color(0x88000000),
+              borderRadius: BorderRadius.circular(6)),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: const <Widget>[CircularProgressIndicator(), Text('正在加载')],
+          ),
+        ),
+      );
+    }
+    if (_focusData!.isEmpty) {
+      return _emptyWidget();
+    }
+    return ListView.builder(
+      itemCount: _focusData!.length,
+      itemBuilder: (contxt, index) {
+        var future =
+            DateTime.fromMillisecondsSinceEpoch(_focusData![index].onlineTime);
+        String time = formatDate(future, [mm, '月', dd, '日']);
+        return InkWell(
+          onTap: () async {
+            if (kDebugMode) {
+              print('到通知详情');
+            }
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => NoticeDetail(product: _focusData![index]),
+              ),
+            );
+          },
+          child: Column(
+            children: [
+              Container(
+                height: 150.h,
+                width: 1.sw,
+                decoration: const BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(30),
+                  ),
+                  // border: Border.all(color: Colors.black54),
+                ),
+                margin: EdgeInsets.only(top: 10.h),
+                child: Stack(
+                  children: <Widget>[
+                    //图片
+                    Positioned(
+                        top: 16.h,
+                        left: 20.w,
+                        child: CircleAvatar(
+                          radius: 70.0.r,
+                          backgroundImage:
+                              NetworkImage(_focusData![index].imgUrl),
+                        )),
+                    //标题
+                    Positioned(
+                      top: 24.0.h,
+                      left: 170.0.w,
+                      child: Text(
+                        _focusData![index].title,
+                        style: const TextStyle(
+                          fontFamily: 'MyFontStyle',
+                          fontSize: 16,
                         ),
-                        // border: Border.all(color: Colors.black54),
                       ),
-                      margin: EdgeInsets.only(top: 10.h),
-                      child: Stack(
-                        children: <Widget>[
-                          //图片
-                          Positioned(
-                              top: 16.h,
-                              left: 20.w,
-                              child: CircleAvatar(
-                                radius: 70.0.r,
-                                backgroundImage:
-                                    NetworkImage(_focusData[index].imgUrl),
-                              )),
-                          //标题
-                          Positioned(
-                            top: 24.0.h,
-                            left: 170.0.w,
-                            child: Text(
-                              _focusData[index].title,
-                              style: const TextStyle(
-                                fontFamily: 'MyFontStyle',
-                                fontSize: 16,
-                              ),
-                            ),
-                          ),
-                          //时间
-                          Positioned(
-                            top: 30.0.h,
-                            left: 580.0.w,
-                            child: Text(
-                              time,
-                              style: const TextStyle(
-                                color: Colors.grey,
-                                fontFamily: 'MyFontStyle',
-                                fontSize: 13,
-                              ),
-                            ),
-                          ),
-                          //介绍
-                          Positioned(
-                            top: 80.h,
-                            left: 180.w,
-                            child: SizedBox(
-                              height: 35,
-                              width: 400.w,
-                              child: Text(
-                                _focusData[index].description, //最多60字
-                                style: const TextStyle(
-                                    color: Colors.grey,
-                                    fontFamily: 'MyFontStyle',
-                                    fontSize: 14),
-                                maxLines: 2,
-                                overflow: TextOverflow.ellipsis,
-                              ),
-                            ),
-                          ),
-                        ],
+                    ),
+                    //时间
+                    Positioned(
+                      top: 30.0.h,
+                      left: 580.0.w,
+                      child: Text(
+                        time,
+                        style: const TextStyle(
+                          color: Colors.grey,
+                          fontFamily: 'MyFontStyle',
+                          fontSize: 13,
+                        ),
+                      ),
+                    ),
+                    //介绍
+                    Positioned(
+                      top: 80.h,
+                      left: 180.w,
+                      child: SizedBox(
+                        height: 35,
+                        width: 400.w,
+                        child: Text(
+                          _focusData![index].description, //最多60字
+                          style: const TextStyle(
+                              color: Colors.grey,
+                              fontFamily: 'MyFontStyle',
+                              fontSize: 14),
+                          maxLines: 2,
+                          overflow: TextOverflow.ellipsis,
+                        ),
                       ),
                     ),
                   ],
                 ),
-              );
-            } else {
-              return Container(
-                width: double.infinity,
-                height: double.infinity,
-                decoration: const BoxDecoration(color: Colors.transparent),
-                alignment: Alignment.center,
-                child: Container(
-                  height: 80,
-                  padding: const EdgeInsets.all(10),
-                  decoration: BoxDecoration(
-                      color: const Color(0x88000000),
-                      borderRadius: BorderRadius.circular(6)),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    children: const <Widget>[
-                      CircularProgressIndicator(),
-                      Text('正在加载')
-                    ],
-                  ),
-                ),
-              );
-            }
-          },
-        ),
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+
+  Widget _emptyWidget() {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 80),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Image.asset("assets/images/notice.png", width: 400.w, height: 400.w),
+          SizedBox(height: 40.w, width: double.infinity),
+          PFtext.text1(text: "您还没有通知哦", fontSize: 13, color: PFc.textSecondary)
+        ],
       ),
     );
   }
